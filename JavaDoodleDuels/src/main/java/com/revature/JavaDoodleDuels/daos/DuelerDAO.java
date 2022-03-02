ab18b4a9db181bd831376432d0920acf6fc20e75
Brandon Clark <brandon.clark.sc@gmail.com>
2022-03-02 11:45:04 -0800

    added annotations

--- JavaDoodleDuels/src/main/java/com/revature/JavaDoodleDuels/daos/DuelerDAO.java
+++ JavaDoodleDuels/src/main/java/com/revature/JavaDoodleDuels/daos/DuelerDAO.java
@@ -1,9 +1,11 @@
 package com.revature.JavaDoodleDuels.daos;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.JavaDoodleDuels.models.Dueler;
 
+@Repository
 public interface DuelerDAO extends CrudRepository<Dueler, String> {
 
 }
--- JavaDoodleDuels/src/main/java/com/revature/JavaDoodleDuels/daos/UserDAO.java
+++ JavaDoodleDuels/src/main/java/com/revature/JavaDoodleDuels/daos/UserDAO.java
@@ -3,9 +3,11 @@ package com.revature.JavaDoodleDuels.daos;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.JavaDoodleDuels.models.User;
 
+@Repository
 public interface UserDAO extends CrudRepository<User, String> {
 
 	Optional<User> findUserbyUsername(String username);
